9fda2432858c7986ee4851b7701c1da828d42daa
ilya11211 <ilya11211@gmail.com>
2016-10-13 22:10:14 +0300

    Added player core subsystem

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using Entitas;
+using Entitas.Unity.Serialization.Blueprints;
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Blueprints blueprints;
+
+    Systems _systems;
+    // Use this for initialization
+    void Start () {
+
+        var pools = Pools.sharedInstance;
+        pools.SetAllPools();
+        pools.AddEntityIndices();
+
+        //pools.blueprints.SetBlueprints(blueprints);
+
+        _systems = createSystems(pools);
+
+        
+
+        _systems.Initialize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy()
+    {
+        _systems.TearDown();
+    }
+
+    Systems createSystems(Pools pools)
+    {
+        return new Feature("Systems");
+
+            // Initialize
+           
+            
+
+           
+
+            // Destroy
+            
+    }
+}
---
+++ Assets/Sources/Features/Input/InputSystem.cs
@@ -0,0 +1,44 @@
+﻿using Entitas;
+using UnityEngine;
+
+public sealed class InputSystem : ISetPools, IExecuteSystem, ICleanupSystem {
+
+    const string PLAYER1_ID = "Player1";
+
+    Pools _pools;
+    Group _moveInputs;
+    Group _shootInputs;
+
+    public void SetPools(Pools pools) {
+        _pools = pools;
+        //_moveInputs = pools.input.GetGroup(InputMatcher.MoveInput);
+        //_shootInputs = pools.input.GetGroup(InputMatcher.ShootInput);
+    }
+
+    public void Execute() {
+        var moveX = Input.GetAxisRaw("Horizontal");
+        var moveY = Input.GetAxisRaw("Vertical");
+
+        /*_pools.input.CreateEntity()
+                 .AddMoveInput(new Vector3(moveX, moveY))
+                 .AddInputOwner(PLAYER1_ID);
+
+        if(Input.GetAxisRaw("Fire1") != 0) {
+            _pools.input.CreateEntity()
+                     .IsShootInput(true)
+                     .AddInputOwner(PLAYER1_ID);
+        }
+
+        _pools.input.isSlowMotion = Input.GetAxisRaw("Fire2") != 0;*/
+    }
+
+    public void Cleanup() {
+        foreach(var e in _moveInputs.GetEntities()) {
+            _pools.input.DestroyEntity(e);
+        }
+
+        foreach(var e in _shootInputs.GetEntities()) {
+            _pools.input.DestroyEntity(e);
+        }
+    }
+}
